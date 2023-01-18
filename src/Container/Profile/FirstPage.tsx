3df540b68653d8d8320d7a4a3a1136bca48f0de2
Vitika9 <vitika.program@gmail.com>
2023-01-18 22:28:16 +0530

    Code-health: Refactored and reformatted FirstPage.tsx and Profile.tsx

--- src/Container/Profile/FirstPage.tsx
+++ src/Container/Profile/FirstPage.tsx
@@ -1,101 +1,96 @@
-import { Backdrop, Box, Button, Divider } from '@mui/material'
-import React, { useState } from 'react'
-import {useNavigate, NavLink } from 'react-router-dom'
+import { Backdrop, Box } from '@mui/material'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './firstpage.css'
 import { Container } from '@mui/system'
 
-export var userdata:any ={}; 
+export var userdata: any = {};
 
 export default function FirstPage() {
-  
-  const[firstname, setfirstName]= useState('');
-  const[lastname, setlastName]= useState('');
-  const[username, setuserName]= useState('');
-  const[email,setEmail]=useState('');
-  const[number,setnumber] = useState('');
-  const[address, setAddress]= useState('');
-  const[arlist,setarlist]=useState({});
-  const [field,setField] = useState('');
-  const[fieldopen,setFieldOpen]=useState(false);
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [username, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [arlist, setarlist] = useState({});
+  const [field, setField] = useState('');
+  const [fieldopen, setFieldOpen] = useState(false);
   const navigate = useNavigate();
-  
-  const handleSubmit=(e:any)=>{
+
+  const handleSubmit = (e: any) => {
     e.preventDefault();
 
-    if(firstname === ''){
+    if (firstname === '') {
       setField('First Name');
-      setFieldOpen(true);
     }
-    else if(lastname === ''){
+    else if (lastname === '') {
       setField('Last Name');
-      setFieldOpen(true);
     }
-    else if(username === ''){
+    else if (username === '') {
       setField('User Name');
-      setFieldOpen(true);
     }
-    else if(email === ''){
+    else if (email === '') {
       setField('Email');
-      setFieldOpen(true);
     }
-    else if(number === ''){
+    else if (number === '') {
       setField('Phone Number');
-      setFieldOpen(true);
     }
-    else if(address === ''){
+    else if (address === '') {
       setField('Address');
-      setFieldOpen(true);
     }
-    else{
-        userdata={
-          F1:firstname,
-        }
-        setarlist(userdata);
-        console.log(userdata);
-        navigate('/profile');
-        console.log(arlist);
+    else {
+      userdata = {
+        F1: firstname,
+      }
+      setarlist(userdata);
+      console.log(userdata);
+      navigate('/profile');
+      console.log(arlist);
+      return;
     }
-}
+    setFieldOpen(true);
+  }
+
   return (
     <div className='fsbody'>
-        <Container maxWidth='sm' className='fbox1'>
-          <div className='ftitle'>User Profile</div>
-          <form className='fbox2' >
-            <div className='alignleft'>First Name<a className='colon'>:
-                  <input className='in' type='text' name='firstname' value={firstname} onChange={(e)=>setfirstName(e.target.value)} required={true} />
-                  </a>
-            </div>
-            <div className='alignleft'>Last Name<a className='colon'>:
-                  <input className='in' type='text' name='lastname' value={lastname} onChange={(e)=>setlastName(e.target.value)} required={true} />
-                  </a>
-                  </div>
-            <div className='alignleft'>User Name<a className='colon'>:
-                  <input className='in' type='text' name='username' value={username} onChange={(e)=>setuserName(e.target.value)} required={true} />
-                  </a>
-                  </div>
-            <div className='alignleft'>Email<a className='colon'>:
-                  <input className='in' type='email' name='email' value={email} onChange={(e)=>setEmail(e.target.value)} required={true} />
-                  </a>
-                  </div>
-            <div className='alignleft'>Phone Number<a className='colon'>:
-                  <input className='in' type='tel' name='number' value={number} onChange={(e)=>setnumber(e.target.value)} required={true} />
-                  </a>
-                  </div>
-            <div className='alignleft'>Address<a className='colon'>:
-                  <input className='in' type='text' name='address' value={address} onChange={(e)=>setAddress(e.target.value)} required={true} />
-                  </a>
-            </div>
-            
-            <button  className='btnup' onClick={handleSubmit} >Submit</button>
+      <Container maxWidth='sm' className='fbox1'>
+        <div className='ftitle'>User Profile</div>
+        <form className='fbox2' >
+          <div className='alignleft'>First Name<a className='colon'>:
+            <input className='in' type='text' name='firstname' value={firstname} onChange={(e) => setFirstName(e.target.value)} required={true} />
+          </a>
+          </div>
+          <div className='alignleft'>Last Name<a className='colon'>:
+            <input className='in' type='text' name='lastname' value={lastname} onChange={(e) => setLastName(e.target.value)} required={true} />
+          </a>
+          </div>
+          <div className='alignleft'>User Name<a className='colon'>:
+            <input className='in' type='text' name='username' value={username} onChange={(e) => setUserName(e.target.value)} required={true} />
+          </a>
+          </div>
+          <div className='alignleft'>Email<a className='colon'>:
+            <input className='in' type='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} required={true} />
+          </a>
+          </div>
+          <div className='alignleft'>Phone Number<a className='colon'>:
+            <input className='in' type='tel' name='number' value={number} onChange={(e) => setNumber(e.target.value)} required={true} />
+          </a>
+          </div>
+          <div className='alignleft'>Address<a className='colon'>:
+            <input className='in' type='text' name='address' value={address} onChange={(e) => setAddress(e.target.value)} required={true} />
+          </a>
+          </div>
+          <button className='btnup' onClick={handleSubmit} >Submit</button>
         </form>
-        
+
       </Container>
-        <Backdrop open={fieldopen}>
-          <Box className='fillbox'>
-            <div className='a1'>Please Fill in the {field}</div>
-            <button className='a2' onClick={()=>setFieldOpen(false)}>Okay</button>
-          </Box>
-        </Backdrop>
+      <Backdrop open={fieldopen}>
+        <Box className='fillbox'>
+          <div className='a1'>Please Fill in the {field}</div>
+          <button className='a2' onClick={() => setFieldOpen(false)}>Okay</button>
+        </Box>
+      </Backdrop>
     </div>
   )
 }
