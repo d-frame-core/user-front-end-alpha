565c3c57e7b9905aa452b274c8653d1fb88c12f4
moayaan1911 <ayaangames@gmail.com>
2023-10-07 07:25:54 +0530

    kyc3 intefgration done

--- src/Container/KYC/KYC3/KYC3.tsx
+++ src/Container/KYC/KYC3/KYC3.tsx
@@ -16,9 +16,73 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../../components/Header/Header';
 import { MyContext } from '../../../components/context/Context';
+import toast from 'react-hot-toast';
+import { useAccount } from 'wagmi';
 
 export default function KYC3() {
-  const { captureImage } = useContext(MyContext);
+  const [governmentProof1, setGovernmentProof1] = useState<any>(null);
+  const [governmentProof2, setGovernmentProof2] = useState<any>(null);
+  const [userPhoto, setUserPhoto] = useState<any>(null);
+  const navigate = useNavigate();
+
+  const { userDataa } = useContext(MyContext);
+  var { address, isConnected }: any = useAccount();
+  const isValidFileExtension = (file: any) => {
+    const allowedExtensions = ['jpeg', 'png'];
+    const fileExtension = file.name.split('.').pop().toLowerCase();
+    return allowedExtensions.includes(fileExtension);
+  };
+  const handleFormSubmit = async (e: any) => {
+    e.preventDefault();
+    toast.loading('Uploading Proof', { id: '1' });
+    if (!governmentProof1 || !governmentProof2 || !userPhoto) {
+      toast.error('Select All files', { id: '1' });
+      return;
+    }
+    if (
+      (governmentProof1 && !isValidFileExtension(governmentProof1)) ||
+      (governmentProof2 && !isValidFileExtension(governmentProof2)) ||
+      (userPhoto && !isValidFileExtension(userPhoto))
+    ) {
+      // Invalid file type(s), show an error toast
+      toast.error('Only JPEG and PNG files are supported', { id: '1' });
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('governmentProof1', governmentProof1);
+    formData.append('governmentProof2', governmentProof2);
+    formData.append('userPhoto', userPhoto);
+
+    try {
+      const publicAddress =
+        localStorage.getItem('userPublicAddress') ||
+        userDataa.publicAddress ||
+        address;
+      await axios
+        .patch(`http://localhost:3000/api/kyc3/${publicAddress}`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          toast.success('Uploaded Image Succesfully', { id: '1' });
+          setTimeout(() => {
+            toast.remove();
+            navigate('/successful');
+          }, 1000);
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error('Error Uploading Image', { id: '1' });
+        });
+    } catch (error) {
+      console.error('Error submitting KYC information:', error);
+      toast.error('Error sdUploading Image', { id: '1' });
+    }
+  };
+
   return (
     <Container>
       <Header />
@@ -36,7 +100,11 @@ export default function KYC3() {
           </span>
           <div className='up'>
             <div className='cbtn'>
-              <CaptureButtons />
+              <input
+                type='file'
+                accept='image/*'
+                onChange={(e: any) => setUserPhoto(e.target.files[0])}
+              />
             </div>
           </div>
         </div>
@@ -51,7 +119,11 @@ export default function KYC3() {
           </span>
           <div className='up'>
             <div className='cbtn'>
-              <UploadButtonForKYC />
+              <input
+                type='file'
+                accept='image/*'
+                onChange={(e: any) => setGovernmentProof1(e.target.files[0])}
+              />
             </div>
             <span
               className='Image-warnings'
@@ -73,7 +145,11 @@ export default function KYC3() {
           </span>{' '}
           <div className='up'>
             <div className='cbtn'>
-              <UploadButtonForKYC />
+              <input
+                type='file'
+                accept='image/*'
+                onChange={(e: any) => setGovernmentProof2(e.target.files[0])}
+              />
             </div>
             <span
               className='Image-warnings'
@@ -98,21 +174,18 @@ export default function KYC3() {
             </Stepper>
           </Box>
         </div>
-        <NavLink
-          style={{ textDecoration: 'none' }}
-          to='/successful'>
-          <Button
-            sx={{
-              backgroundColor: '#017EFA',
-              borderRadius: '10px',
-              textTransform: 'inherit',
-              width: '100px',
-            }}
-            variant='contained'
-            className='nextbtn'>
-            Submit
-          </Button>
-        </NavLink>
+        <Button
+          sx={{
+            backgroundColor: '#017EFA',
+            borderRadius: '10px',
+            textTransform: 'inherit',
+            width: '100px',
+          }}
+          variant='contained'
+          className='nextbtn'
+          onClick={handleFormSubmit}>
+          Submit
+        </Button>
 
         <NavLink
           style={{ textDecoration: 'none' }}
@@ -133,176 +206,3 @@ export default function KYC3() {
     </Container>
   );
 }
-
-// import React, { useContext, useState } from 'react';
-// import { Box, Step, StepLabel, Stepper, Button } from '@mui/material';
-// import { Container } from '@mui/system';
-// import { NavLink } from 'react-router-dom';
-// import Sidebar1 from '../../../components/sidebar1/Sidebar1';
-// import UploadButtonForKYC from '../../../components/upload/UploadButtonForKYC';
-// import CaptureButtons from '../../../components/upload/CaptureButton';
-// import data from '../data';
-// import './kyc3.css';
-// import Drawer from '../../../components/sidebar1/Drawer';
-// import axios from 'axios';
-// import { useNavigate } from 'react-router-dom';
-// import Header from '../../../components/Header/Header';
-// import { MyContext } from '../../../components/context/Context';
-// import { useAccount } from 'wagmi';
-
-// // Define your KYC3 component
-// export default function KYC3() {
-//   const [photo, setPhoto] = useState<any>(null);
-//   const [governmentProof1, setGovernmentProof1] = useState<any>(null);
-//   const [governmentProof2, setGovernmentProof2] = useState<any>(null);
-//   const { userDataa, setUserData } = useContext(MyContext);
-//   var { address, isConnected }: any = useAccount();
-//   const navigate = useNavigate();
-
-//   // Function to handle file input change for KYC images
-//   const handleImageChange = (event: any, imageType: any) => {
-//     const file = event.target.files[0];
-//     if (file) {
-//       // Depending on the image type, set the state accordingly
-//       if (imageType === 'photo') {
-//         setPhoto(file);
-//       } else if (imageType === 'governmentProof1') {
-//         setGovernmentProof1(file);
-//       } else if (imageType === 'governmentProof2') {
-//         setGovernmentProof2(file);
-//       }
-//     }
-//   };
-
-//   // Function to upload KYC images
-//   const uploadKYCImages = async () => {
-//     try {
-//       const formData = new FormData();
-//       formData.append('photo', photo);
-//       formData.append('governmentProof1', governmentProof1);
-//       formData.append('governmentProof2', governmentProof2);
-//       const publicAddress =
-//         localStorage.getItem('userAddress') ||
-//         userDataa.publicAddress ||
-//         address;
-
-//       // Make a PATCH request to upload the images
-//       await axios.patch(`http://localhost/api/kyc/${publicAddress}`, formData, {
-//         headers: {
-//           'Content-Type': 'multipart/form-data',
-//         },
-//       });
-
-//       // Images uploaded successfully, you can navigate to the next page
-//       alert('Submitted KYC3 data successfully');
-//       setTimeout(() => {
-//         navigate('/successful');
-//       }, 1000);
-//     } catch (error) {
-//       // Handle error
-//       alert('Error submitting KYC3 data');
-//       setTimeout(() => {
-//         navigate('/');
-//       }, 1000);
-//       console.error('Error uploading KYC images:', error);
-//     }
-//   };
-
-//   return (
-//     <Container>
-//       <Header />
-//       <Sidebar1 />
-//       <a className='smopen'>{Drawer(0)}</a>
-//       <Box className='ky2rectangle'>
-//         {/* ... (Your existing JSX code) */}
-
-//         {/* File input for KYC images */}
-//         <div
-//           className='k1'
-//           style={{ fontSize: '24px', fontWeight: 500 }}>
-//           Government Verification 1:
-//           <br />
-//           <span
-//             style={{ fontSize: '0.9rem', textAlign: 'left', fontWeight: 400 }}>
-//             Passport, Aadhar Card, Driving License, Voter ID, PAN Card etc.
-//           </span>
-//           <div className='up'>
-//             <div className='cbtn'>
-//               <input
-//                 type='file'
-//                 accept='.jpg, .png'
-//                 onChange={(e) => handleImageChange(e, 'governmentProof1')}
-//               />
-//             </div>
-//             <span
-//               className='Image-warnings'
-//               style={{ fontSize: '0.8rem', fontWeight: 400 }}>
-//               <span style={{ color: 'red' }}>*</span>
-//               Upload your images less than or equal to
-//               <b style={{ color: 'red' }}> 1 MB</b> in .jpg or .png format
-//             </span>
-//           </div>
-//         </div>
-
-//         {/* File input for KYC images */}
-//         <div
-//           className='k2'
-//           style={{ fontSize: '24px', fontWeight: 500 }}>
-//           Government Verification 2:
-//           <br />
-//           <span
-//             style={{ fontSize: '0.9rem', textAlign: 'left', fontWeight: 400 }}>
-//             Electricity Bill, Phone Bill etc.
-//           </span>{' '}
-//           <div className='up'>
-//             <div className='cbtn'>
-//               <input
-//                 type='file'
-//                 accept='.jpg, .png'
-//                 onChange={(e) => handleImageChange(e, 'governmentProof2')}
-//               />
-//             </div>
-//             <span
-//               className='Image-warnings'
-//               style={{ fontSize: '0.8rem', fontWeight: 400 }}>
-//               <span style={{ color: 'red' }}>*</span>
-//               Upload your images less than or equal to
-//               <b style={{ color: 'red' }}> 1 MB</b> in .jpg or .png format
-//             </span>
-//           </div>
-//         </div>
-
-//         {/* Submit button */}
-//         <div className='stepper-3'>
-//           <Button
-//             sx={{
-//               backgroundColor: '#017EFA',
-//               borderRadius: '10px',
-//               textTransform: 'inherit',
-//               width: '100px',
-//             }}
-//             variant='contained'
-//             className='nextbtn'
-//             onClick={uploadKYCImages}>
-//             Submit
-//           </Button>
-//         </div>
-//         <NavLink
-//           style={{ textDecoration: 'none' }}
-//           to='/kyc2'>
-//           <Button
-//             sx={{
-//               backgroundColor: '#017EFA',
-//               borderRadius: '10px',
-//               textTransform: 'inherit',
-//               width: '100px',
-//             }}
-//             variant='contained'
-//             className='backbtn'>
-//             Back
-//           </Button>
-//         </NavLink>
-//       </Box>
-//     </Container>
-//   );
-// }
